45dc73d73b291bf79d918ee51c644361736321f6
bima <bimajatiwijaya@gmail.com>
2017-05-27 16:29:39 +0700

    add model needed from crm_claim.

--- rma/models/crm_claim.py
+++ rma/models/crm_claim.py
@@ -87,7 +87,8 @@ class CrmClaim(models.Model):
                                      'crm.claim'))
     partner_id = fields.Many2one('res.partner', 'Partner')
     email_cc = fields.Text('Watchers Emails', size=252,
-                           help="These email addresses will be added to the CC field of all inbound and outbound emails for this record before being sent. Separate multiple email addresses with a comma")
+                           help="These email addresses will be added to the CC field of all inbound and outbound "
+                           " emails for this record before being sent. Separate multiple email addresses with a comma")
     email_from = fields.Char('Email', size=128, help="Destination email for email gateway.")
     partner_phone = fields.char('Phone')
     stage_id = fields.Many2one('crm.claim.stage', 'Stage', track_visibility='onchange',
@@ -356,3 +357,46 @@ class CrmClaim(models.Model):
 
         std_default.update(default)
         return super(CrmClaim, self).copy(default=std_default)
+
+
+class CrmClaimCategory(models.Model):
+    _name = "crm.claim.category"
+    _description = "Category of claim"
+
+    name = fields.Char('Name', required=True, translate=True)
+    team_id = fields.Many2one('crm.team', 'Sales Team')
+
+
+class CrmClaimStage(models.Model):
+    """ Model for claim stages. This models the main stages of a claim
+        management flow. Main CRM objects (leads, opportunities, project
+        issues, ...) will now use only stages, instead of state and stages.
+        Stages are for example used to display the kanban view of records.
+    """
+    _name = "crm.claim.stage"
+    _description = "Claim stages"
+    _rec_name = 'name'
+    _order = "sequence"
+
+    name = fields.Char('Stage Name', required=True, translate=True)
+    sequence = fields.Integer('Sequence', help="Used to order stages. Lower is better.", default=1)
+    team_ids = fields.Many2many('crm.team', 'crm_team_claim_stage_rel',
+                                'stage_id', 'team_id', string='Teams',
+                                help="Link between stages and sales teams. When set, "
+                                "this limitate the current stage to the selected sales teams.")
+    case_default = fields.boolean('Common to All Teams',
+                                  help="If you check this field, this stage will be proposed by default "
+                                  " on each sales team. It will not assign this stage to existing teams.")
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.multi
+    def _claim_count(self):
+        claim = self.env['crm.claim']
+        for partner in self:
+            partner.claim_count = claim.search_count(
+                ['|', ('partner_id', 'in', partner.child_ids.ids), ('partner_id', '=', partner.id)])
+
+    claim_count = fields.Integer(compute='_claim_count', string='# Claims')
