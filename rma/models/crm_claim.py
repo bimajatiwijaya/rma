5c2e4030ed0cb02a8afec0f7b3efcfd45a0e4af3
bima <bimajatiwijaya@gmail.com>
2017-05-27 13:51:20 +0700

    - migrade crm_claim object to new API - merged with rma module

--- rma/models/crm_claim.py
+++ rma/models/crm_claim.py
@@ -4,14 +4,144 @@
 # © 2009-2013 Akretion,
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import _, api, exceptions, fields, models
+from odoo import _, api, exceptions, fields, models
 
+from odoo.tools import html2plaintext
 from .invoice_no_date import InvoiceNoDate
 from .product_no_supplier import ProductNoSupplier
 
 
 class CrmClaim(models.Model):
-    _inherit = 'crm.claim'
+    _name = "crm.claim"
+    _description = "Claim"
+    _order = "priority,date desc"
+    _inherit = ['mail.thread']
+
+    @api.model
+    @api.returns('self', lambda value: value.id if value else False)
+    def _get_default_stage_id(self):
+        """ Gives default stage_id """
+        team_id = self.env['crm.team']._get_default_team_id(user_id=self.env.uid)
+        return self.stage_find([], team_id, [('sequence', '=', '1')])
+
+    id = fields.Integer('ID', readonly=True)
+    name = fields.Char('Claim Subject', required=True)
+    active = fields.Boolean('Active', default=True)
+    action_next = fields.Char('Next Action')
+    date_action_next = fields.Datetime('Next Action Date')
+    description = fields.Text('Description')
+    resolution = fields.Text('Resolution')
+    create_date = fields.Datetime('Creation Date', readonly=True)
+    write_date = fields.Datetime('Update Date', readonly=True)
+    date_deadline = fields.Date('Deadline')
+    date_closed = fields.Datetime('Closed', readonly=True)
+    date = fields.Datetime('Claim Date', select=True, default=fields.Datetime.now)
+    ref = fields.Reference('Reference', selection=odoo.addons.base.res.res_request.referencable_models)
+    categ_id = fields.Many2one('crm.claim.category', 'Category')
+    priority = fields.Selection([('0', 'Low'), ('1', 'Normal'), ('2', 'High')], 'Priority', default='1')
+    type_action = fields.Selection([('correction', 'Corrective Action'), ('prevention', 'Preventive Action')],
+                                   'Action Type')
+    user_id = fields.Many2one('res.users', 'Responsible', track_visibility='always', default=lambda self: self.env.uid)
+    user_fault = fields.Char('Trouble Responsible')
+    team_id = fields.Many2one('crm.team', 'Sales Team', oldname='section_id',
+                              select=True, help="Responsible sales team."
+                                                " Define Responsible user and Email account for"
+                                                " mail gateway.",
+                              default=lambda self: self.env['crm.team']._get_default_team_id())
+    company_id = fields.Many2one('res.company', 'Company',
+                                 default=lambda s: s.env['res.company']._company_default_get('crm.case'))
+    partner_id = fields.Many2one('res.partner', 'Partner')
+    email_cc = fields.Text('Watchers Emails', size=252,
+                           help="These email addresses will be added to the CC field of all inbound and outbound emails for this record before being sent. Separate multiple email addresses with a comma")
+    email_from = fields.Char('Email', size=128, help="Destination email for email gateway.")
+    partner_phone = fields.char('Phone')
+    stage_id = fields.Many2one('crm.claim.stage', 'Stage', track_visibility='onchange',
+                               domain="['|', ('team_ids', '=', team_id), ('case_default', '=', True)]",
+                               default=lambda s: s._get_default_stage_id())
+    cause = fields.Text('Root Cause')
+
+    @api.model
+    def stage_find(self, cases, team_id, domain=[], order='sequence'):
+        """ Override of the base.stage method
+            Parameter of the stage search taken from the lead:
+            - team_id: if set, stages must belong to this team or
+              be a default case
+        """
+        if isinstance(cases, (int, long)):
+            cases = self.browse(cases)
+        # collect all team_ids
+        team_ids = []
+        if team_id:
+            team_ids.append(team_id)
+        for claim in cases:
+            if claim.team_id:
+                team_ids.append(claim.team_id.id)
+        # OR all team_ids and OR with case_default
+        search_domain = []
+        if team_ids:
+            search_domain += [('|')] * len(team_ids)
+            for team_id in team_ids:
+                search_domain.append(('team_ids', '=', team_id))
+        search_domain.append(('case_default', '=', True))
+        # AND with the domain in parameter
+        search_domain += list(domain)
+        # perform search, return the first found
+        stage_ids = self.env['crm.claim.stage'].search(search_domain, order=order)
+        if stage_ids:
+            return stage_ids[0]
+        return False
+
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        """This function returns value of partner address based on partner
+           :param email: ignored
+        """
+        if not self.partner_id:
+            return {'value': {'email_from': False, 'partner_phone': False}}
+        address = self.env['res.partner'].browse([self.partner_id.id])
+        return {'value': {'email_from': address.email, 'partner_phone': address.phone}}
+
+    @api.model
+    def create(self, vals):
+        context = self._context.copy()
+        if vals.get('team_id') and not context.get('default_team_id'):
+            context['default_team_id'] = vals.get('team_id')
+
+        # context: no_log, because subtype already handle this
+        return super(CrmClaim, self).with_context(context).create(vals)
+
+    @api.multi
+    def copy(self, default=None):
+        self.ensure_one()
+        default = dict(default or {})
+        default['name'] = self.name
+        default['stage_id'] = self._get_default_stage_id()
+        return super(CrmClaim, self).copy(default=default)
+
+    # -------------------------------------------------------
+    # Mail gateway
+    # -------------------------------------------------------
+    @api.model
+    def message_new(self, msg, custom_values=None):
+        """ Overrides mail_thread message_new that is called by the mailgateway
+            through message_process.
+            This override updates the document according to the email.
+        """
+        create_context = dict(self.env.context or {})
+        desc = html2plaintext(msg.get('body')) if msg.get('body') else ''
+        defaults = {
+            'name': msg.get('subject') or _("No Subject"),
+            'description': desc,
+            'email_from': msg.get('from'),
+            'email_cc': msg.get('cc'),
+            'partner_id': msg.get('author_id', False),
+        }
+        if custom_values:
+            defaults.update(custom_values)
+        if msg.get('priority'):
+            defaults['priority'] = msg.get('priority')
+        defaults.update(custom_values)
+        return super(CrmClaim, self.with_context(create_context)).message_new(msg, custom_values=defaults)
 
     def _get_default_warehouse(self):
         company_id = self.env.user.company_id.id
